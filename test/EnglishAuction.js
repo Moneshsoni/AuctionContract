3694cc74cc0bd69595b3ff2b72cf4a90cea1b384
Moneshsoni <sonimonesh115@gmail.com>
2023-06-19 20:05:18 +0530

    add the english auction contract

--- test/EnglishAuction.js
+++ test/EnglishAuction.js
@@ -2,23 +2,30 @@ const {
   loadFixture,
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { expect } = require("chai");
-
-describe("MyToken contract", function () {
+const { time } = require('@nomicfoundation/hardhat-network-helpers');
+const {ethers} = require("hardhat")
+describe("EnglishAuction contract", function () {
   async function deployTokenFixture() {
-    let blockTime = (await ethers.provider.getBlock('latest')).timestamp;
+    const timestamp = await time.latest();
   
     let startingBid = 100;
-    let endTime = blockTime + 1000;
+    let endTime = timestamp + 1000;
     let tokenId = 4;
-    
+
     const [owner, addr1, addr2] = await ethers.getSigners();
 
     //Deploy erc721 contract
     const myToken = await ethers.deployContract("MyToken");
 
+    await myToken.safeMint(owner.address,tokenId);
+
     //Deploy EnglishAuction contract
     const EnglishAuction  = await ethers.deployContract("EnglishAuction",[myToken.target,tokenId,startingBid,endTime]);
     // Fixtures can return anything you consider useful for your tests
+   
+    await myToken.approve(EnglishAuction.target,tokenId);
+
+    // await myToken.approve(EnglishAuction.target)
     return { myToken, EnglishAuction, owner, addr1, addr2 };
   }
 
@@ -34,9 +41,43 @@ describe("MyToken contract", function () {
     console.log("Owner of Token",await myToken.owner());
   })
 
-  it("Should check the englishAuctionContract",async function(){
-    const { EnglishAuction,owner} = await loadFixture(deployTokenFixture);
-    console.log("English Auction",EnglishAuction.target);
+  it("Shoud Start English Auction",async function(){
+    const { myToken,EnglishAuction,owner} = await loadFixture(deployTokenFixture);
+
+    // await myToken.safeMint(owner.address,tokenId);
+    await EnglishAuction.connect(owner).start();
+    console.log("Balance of",await myToken.balanceOf(EnglishAuction.target));
+
+    console.log("Owner of Token",await myToken.ownerOf(4));
+    // console.log("Owner address!",await owner.address);
+    // console.log("EnglishAuction!",await EnglishAuction.target);
+
+  })
+
+  it("Should check bid function not started",async function (){
+    const {EnglishAuction} = await loadFixture(deployTokenFixture);
+   
+    await expect( EnglishAuction.bid()).to.be.revertedWith("not started");
+  })
+
+  it("Should check bid functions value < highest! ",async function (){
+    const {EnglishAuction,owner} = await loadFixture(deployTokenFixture);
+    
+    await EnglishAuction.connect(owner).start();
+
+    await expect(EnglishAuction.bid()).to.be.revertedWith("value < highest");
+  })
+
+  it("Should check the Bid Functions",async function (){
+    const {EnglishAuction,add1,owner} = await loadFixture(deployTokenFixture);
+   
+    await EnglishAuction.connect(owner).start();
+
+    const valueToSend = ethers.utils.parseEther('1.0');
+
+    await EnglishAuction.connect(add1).bid({ value: valueToSend });
+
+
   })
 
 });
